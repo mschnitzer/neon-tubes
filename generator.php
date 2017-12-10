a7a6023de15146b55ecbf874ee2a3676decb6b53
Manuel Schnitzer <webmaster@mschnitzer.de>
2017-12-10 11:49:56 +0100

    Use always lowercase characters for file names

--- generator.php
+++ generator.php
@@ -4,7 +4,7 @@
  $color = $_GET['color'];
  header('Content-Description: File Transfer');
  header('Content-Type: application/octet-stream');
- header('Content-Disposition: attachment; filename=neon_tube_'.$color.'.dff');
+ header('Content-Disposition: attachment; filename=neon_tube_'.strtolower($color).'.dff');
  header('Content-Length: '.filesize('RedNeonTube1.dff'));
  echo hex2bin(str_replace('ff0000', $color, bin2hex(file_get_contents('RedNeonTube1.dff'))));
 ?>
