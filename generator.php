4e7861342d46335196ca70ec19ae4d73e40c4e6c
A M <32740481+am9029@users.noreply.github.com>
2017-12-10 03:27:49 +0000

    Update generator.php
    Optimised slightly

--- generator.php
+++ generator.php
@@ -1,33 +1,10 @@
 <?php
- if (!isset($_GET['color']) || strlen($_GET['color']) != 6) {
-   echo "no hex color provided. example: generator.php?color=ff0000";
-   exit;
- }
-
+ if (!isset($_GET['color']) || !preg_match("/[\da-f]{6}/i", $_GET['color']))
+   die('no hex color provided. example: generator.php?color=ff0000');
  $color = $_GET['color'];
-
- for ($i = 0; $i < strlen($color); $i++) {
-   if (!is_numeric($color[$i]) && $color[$i] != 'a' && $color[$i] != 'b' && $color[$i] != 'c' && $color[$i] != 'd' &&
-     $color[$i] != 'e' && $color[$i] != 'f') {
-       echo "no hex color provided. example: generator.php?color=ff0000";
-       exit;
-   }
- }
-
- define('DIR', dirname(__FILE__));
- $file = DIR.'/tmp/'.$color;
-
- if (!file_exists($file)) {
-   $hex = bin2hex(file_get_contents('RedNeonTube1.dff'));
-   $hex = str_replace('ff0000', $color, $hex);
-
-   file_put_contents($file, hex2bin($hex));
- }
-
  header('Content-Description: File Transfer');
  header('Content-Type: application/octet-stream');
  header('Content-Disposition: attachment; filename=neon_tube_'.$color.'.dff');
- header('Content-Length: '.filesize($file));
- readfile($file);
+ header('Content-Length: '.filesize('RedNeonTube1.dff'));
+ echo hex2bin(str_replace('ff0000', $color, bin2hex(file_get_contents('RedNeonTube1.dff'))));
 ?>
-
